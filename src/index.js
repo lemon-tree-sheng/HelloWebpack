37aaf32f1b58314c111a0772ee9211db0ad45c26
shengxingyue <shengxingyue@souche.com>
2017-10-15 12:02:02 +0800

    处理 css 文件

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 * @Author: shengxingyue
 * @Date:   2017-10-14 20:03:39
 * @Last Modified by:   shengxingyue
-* @Last Modified time: 2017-10-15 11:31:58
+* @Last Modified time: 2017-10-15 11:47:01
 */
 import _ from 'lodash';
+import './style.css';
 function component() {
 	var element = document.createElement('div');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
 
 	return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: shengxingyue
 * @Date:   2017-10-14 20:15:30
 * @Last Modified by:   shengxingyue
-* @Last Modified time: 2017-10-15 11:23:03
+* @Last Modified time: 2017-10-15 11:52:26
 */
 const config = {
 	entry: './src/index.js',
@@ -10,6 +10,12 @@ const config = {
 	output: {
 		path: __dirname + '/dist/',
 		filename: 'bundle.js'
+	},
+
+	module : {
+		rules : [
+			{test: /\.css$/, use: ['style-loader','css-loader']},
+		]
 	}
 };
 module.exports = config;
