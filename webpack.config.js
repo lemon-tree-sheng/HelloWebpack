9b037a18ce923a8ee2c41d55c8120edb1c00a579
shengxingyue <shengxingyue@souche.com>
2017-10-15 12:57:30 +0800

    使用 ExtractTextWebpackPlugin 分离 css 文件

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@
 * @Author: shengxingyue
 * @Date:   2017-10-14 20:15:30
 * @Last Modified by:   shengxingyue
-* @Last Modified time: 2017-10-15 11:52:26
+* @Last Modified time: 2017-10-15 12:49:42
 */
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const config = {
 	entry: './src/index.js',
 
@@ -12,10 +13,17 @@ const config = {
 		filename: 'bundle.js'
 	},
 
-	module : {
+	module: {
 		rules : [
-			{test: /\.css$/, use: ['style-loader','css-loader']},
+			{test: /\.css$/, use: ExtractTextPlugin.extract({
+          			fallback: "style-loader",
+          			use: "css-loader"
+        	})}
 		]
-	}
+	},
+
+	plugins: [
+		new ExtractTextPlugin('style.css')
+	]
 };
 module.exports = config;
